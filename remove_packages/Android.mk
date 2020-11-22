f3a6bd3425014d4f0308e5c50ab74c48182f1458
IAmSalvaMartini <starraos@gmail.com>
2020-11-22 18:41:54 +0530

    vince:Nuke some more gapps to reduce build size
    Signed-off-by: IAmSalvaMartini <starraos@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt CarrierSetup MyVerizonServices OBDM_Permissions OPScreenRecord SprintDM SprintHM VzwOmaTrigger
+ LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt CarrierSetup FM2 MyVerizonServices OBDM_Permissions OPScreenRecord SprintDM SprintHM YouTube YouTubeMusicPrebuilt Drive SafetyHubPrebuilt RecorderPrebuilt CalendarGooglePrebuilt VzwOmaTrigger
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
