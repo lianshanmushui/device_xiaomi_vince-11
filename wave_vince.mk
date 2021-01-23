26d14408c4174388624d683c80405dc9bbd8d5d2
aseelps <aseelpsathar@gmail.com>
2021-01-24 03:08:57 +0530

    vince: Bringup WaveOS

--- wave_vince.mk
+++ wave_vince.mk
@@ -21,14 +21,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from vince device
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
-# Inherit some common octavi stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+# Inherit some common wave stuff.
+$(call inherit-product, vendor/wave/configs/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
-PRODUCT_NAME := aosp_vince
+PRODUCT_NAME := wave_vince
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5 Plus
 PRODUCT_MANUFACTURER := Xiaomi
